0d20763e748bee58cfab90f3778b468485442cbd
Ashu7567 <74881968+Ashu7567@users.noreply.github.com>
2025-03-02 23:10:19 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -9,10 +9,7 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 
 // MongoDB Connection
-mongoose.connect('mongodb://localhost:27017/employeeDB', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
+mongoose.connect('mongodb://localhost:27017/employeeDB')
 .then(() => console.log('MongoDB connected'))
 .catch(err => console.error('MongoDB connection error:', err));
 
